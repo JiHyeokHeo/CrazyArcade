845e04b63a8f366b01bc3b25ee56df7cbca07d57
HEOJIHYEOK <HEOJIHYEOK@DESKTOP-R2NBB1B>
2023-02-28 18:45:03 +0900

    resources

--- CrazyAracde/Client/tyBazzi.cpp
+++ CrazyAracde/Client/tyBazzi.cpp
@@ -16,11 +16,20 @@ namespace ty
 	}
 	void Bazzi::Initialize()
 	{
-		Image* mImage = Resources::Load<Image>(L"Bazzi", L"..\\Resources\\right.bmp");
+		Image* mImage = Resources::Load<Image>(L"Bazzi", L"..\\Resources\\Bazzi.bmp");
 		Animator* animator = AddComponent<Animator>();
-		animator->CreateAnimation(L"right", mImage, Vector2::Zero, 6, 1, 6, Vector2::Zero, 0.1);
+		
+		animator->CreateAnimation(L"up", mImage, Vector2::Zero, 8, 4, 8, Vector2::Zero, 0.1);
+		animator->CreateAnimation(L"right", mImage, Vector2(0.0f, 150.0f), 8, 4, 6, Vector2::Zero, 0.1);
+		animator->CreateAnimation(L"down", mImage, Vector2(0.0f, 150.0f * 2), 8, 4, 8, Vector2::Zero, 0.1);
+		animator->CreateAnimation(L"left", mImage, Vector2(0.0f, 150.0f * 3), 8, 4, 6, Vector2::Zero, 0.1);
 
-		animator->Play(L"right", false);
+		animator->CreateAnimation(L"upIdle", mImage, Vector2::Zero, 8, 4, 1, Vector2::Zero, 0.1);
+		animator->CreateAnimation(L"rightIdle", mImage, Vector2(0.0f, 150.0f), 8, 4, 1, Vector2::Zero, 0.1);
+		animator->CreateAnimation(L"downIdle", mImage, Vector2(0.0f, 150.0f * 2), 8, 4, 1, Vector2::Zero, 0.1);
+		animator->CreateAnimation(L"leftIdle", mImage, Vector2(0.0f, 150.0f * 3), 8, 4, 1, Vector2::Zero, 0.1);
+
+		animator->Play(L"downIdle", false);
 		GameObject::Initialize();
 
 	}
@@ -31,14 +40,37 @@ namespace ty
 		Transform* tr = GetComponent<Transform>();
 		Vector2 pos = tr->GetPos();
 
+		Animator* animator = GetComponent<Animator>();
 		if (Input::GetKeyState(eKeyCode::LEFT) == eKeyState::Pressed)
 		{
-			pos.x -= 100.0f * Time::DeltaTime();
+			pos.x -= 200.0f * Time::DeltaTime();
+			animator->Play(L"left", true);
+		}
+
+		if (Input::GetKeyState(eKeyCode::LEFT) == eKeyState::Down)
+		{
+			animator->Play(L"left", true);
+		}
+		
+		if (Input::GetKeyState(eKeyCode::LEFT) == eKeyState::Up)
+		{
+			animator->Play(L"leftIdle", false);
 		}
 
 		if (Input::GetKeyState(eKeyCode::RIGHT) == eKeyState::Pressed)
 		{
-			pos.x += 100.0f * Time::DeltaTime();
+			pos.x += 200.0f * Time::DeltaTime();
+			animator->Play(L"right", true);
+		}
+
+		if (Input::GetKeyState(eKeyCode::RIGHT) == eKeyState::Down)
+		{
+			animator->Play(L"right", true);
+		}
+
+		if (Input::GetKeyState(eKeyCode::RIGHT) == eKeyState::Up)
+		{
+			animator->Play(L"rightIdle", false);
 		}
 
 		//if (Input::GetKeyState(eKeyCode::SPACEBAR) == eKeyState::Down)
@@ -53,12 +85,34 @@ namespace ty
 
 		if (Input::GetKeyState(eKeyCode::UP) == eKeyState::Pressed)
 		{
-			pos.y -= 100.0f * Time::DeltaTime();
+			pos.y -= 200.0f * Time::DeltaTime();
+			animator->Play(L"up", true);
+		}
+
+		if (Input::GetKeyState(eKeyCode::UP) == eKeyState::Down)
+		{
+			animator->Play(L"up", true);
+		}
+
+		if (Input::GetKeyState(eKeyCode::UP) == eKeyState::Up)
+		{
+			animator->Play(L"upIdle", false);
 		}
 
 		if (Input::GetKeyState(eKeyCode::DOWN) == eKeyState::Pressed)
 		{
-			pos.y += 100.0f * Time::DeltaTime();
+			pos.y += 200.0f * Time::DeltaTime();
+			animator->Play(L"down", true);
+		}
+
+		if (Input::GetKeyState(eKeyCode::DOWN) == eKeyState::Down)
+		{
+			animator->Play(L"down", true);
+		}
+
+		if (Input::GetKeyState(eKeyCode::DOWN) == eKeyState::Up)
+		{
+			animator->Play(L"downIdle", false);
 		}
 		tr->SetPos(pos);
 	}
